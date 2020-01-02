7e0a8b4101cd7a8f4d5bc48afbdbf946f6b38a97
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-01-02 17:18:40 +0600

    Создал и закончил второй компонент компонент

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import Countries from "./Components/Countries/Countries";
 
 class App extends Component{
-  render() {
-    return (
-        <div className="App">
-
-        </div>
-    )
-  }
+    render() {
+        return(
+            <div className="App">
+                <Countries/>
+            </div>
+        )
+    }
 }
 
 export default App;
